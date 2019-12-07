c1e9b65fa66f830a20fd040efca86e00bc2cc9e9
YakovSPb <yakov_dux@bk.ru>
2019-12-07 23:03:52 +0300

    Main page desktop is ready

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,64 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+// CLIENT SLIDER START
+	$('.clients-slider').slick({
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	speed: 900,
+	infinite: true,
+	arrows: false,
+	responsive: [
+	{
+		breakpoint: 1379,
+		settings: {
+			slidesToShow: 1
+		}
+	}
+	]
+});
+// CLIENT SLIDER END
+
+// CASE SLIDER START
+	$('.cases-slider').slick({
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	draggable: true,
+	speed: 900,
+	rows: 2,
+	infinite: true,
+	arrows: false,
+	centerMode: true,
+	centerPadding: '250px',
+	autoplay: true,
+	autoplaySpeed: 9000,
+	responsive: [
+	{
+		breakpoint: 1379,
+		settings: {
+			slidesToShow: 4
+		}
+	}
+	]
+});
+// CASE SLIDER END
+
+// CASE SLIDER START
+	$('.comments').slick({
+	slidesToShow: 1,
+	speed: 900,
+	infinite: true,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 9000,
+	responsive: [
+	{
+		breakpoint: 1379,
+		settings: {
+			slidesToShow: 4
+		}
+	}
+	]
+});
+// CASE SLIDER END
 
 });
