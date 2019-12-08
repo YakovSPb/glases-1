c040301b897964f0f175b57848a718d7caaba837
YakovSPb <yakov_dux@bk.ru>
2019-12-08 22:06:12 +0300

    Home page is ready

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 // CLIENT SLIDER START
-	$('.clients-slider').slick({
+$('.clients-slider').slick({
 	slidesToShow: 4,
 	slidesToScroll: 4,
 	speed: 900,
@@ -9,9 +9,10 @@ document.addEventListener("DOMContentLoaded", function() {
 	arrows: false,
 	responsive: [
 	{
-		breakpoint: 1379,
+		breakpoint: 1200,
 		settings: {
-			slidesToShow: 1
+			slidesToShow: 2,
+			slidesToScroll: 2
 		}
 	}
 	]
@@ -19,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
 // CLIENT SLIDER END
 
 // CASE SLIDER START
-	$('.cases-slider').slick({
+$('.cases-slider').slick({
 	slidesToShow: 4,
 	slidesToScroll: 4,
 	draggable: true,
@@ -28,37 +29,70 @@ document.addEventListener("DOMContentLoaded", function() {
 	infinite: true,
 	arrows: false,
 	centerMode: true,
-	centerPadding: '250px',
+	centerPadding: '150px',
 	autoplay: true,
 	autoplaySpeed: 9000,
 	responsive: [
 	{
 		breakpoint: 1379,
 		settings: {
-			slidesToShow: 4
+			slidesToShow: 2,
+			rows: 1,
+			centerMode: false,
+			centerPadding: '0'
+		}
+	},
+	{
+		breakpoint: 780,
+		settings: {
+			slidesToShow: 1,
+			rows: 1,
+			centerMode: false,
+			centerPadding: '0'
 		}
 	}
 	]
 });
 // CASE SLIDER END
+// var mediaQuery = window.matchMedia("screen and (max-width: 1379x)");
+// mediaQuery.addListener(reinitSlider);
+// foo(mediaQuery);
+
+// function reinitSlider(mq) {
+// 	if(mq){
+// 		$('.cases-slider').slick('reinit')
+// 	}
+// }
+
+
 
 // CASE SLIDER START
-	$('.comments').slick({
+$('.comments').slick({
 	slidesToShow: 1,
 	speed: 900,
 	infinite: true,
 	arrows: false,
 	autoplay: true,
 	autoplaySpeed: 9000,
-	responsive: [
-	{
-		breakpoint: 1379,
-		settings: {
-			slidesToShow: 4
-		}
-	}
-	]
 });
 // CASE SLIDER END
 
-});
+// MENU START
+const link = document.getElementsByClassName('gumburger')
+const close = document.getElementsByClassName('menu__item--close')
+const menu = document.getElementsByClassName('menu')
+
+link[0].addEventListener('click', event => {
+	event.preventDefault()
+	menu[0].classList.add('menu--active')
+	link[0].style.visibility = 'hidden'
+
+})
+close[0].addEventListener('click', event => {
+	event.preventDefault()
+	menu[0].classList.remove('menu--active')
+	link[0].style.visibility = 'visible'
+})
+// MENU END
+
+}) //EDN NAG
